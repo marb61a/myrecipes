586b0a51bc4d4df95066e1927458c5c2ac5dd0d0
Martin Byrne <marb61a@gmail.com>
2015-04-03 12:23:09 +0000

    added new recipes form and error messages

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,4 +8,25 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
   end
   
+  def new
+    @recipe = Recipe.new
+  end
+  
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.chef = Chef.find(1)
+    
+    if @recipe.save
+      flash[:success] = "Your recipe was created successfully"
+      redirect_to recipes_path
+    else
+      render :new
+    end
+  end
+  
+  private 
+    def recipe_params
+      params.require(:recipe).permit(:name, :summary, :description)
+    end
+  
 end
