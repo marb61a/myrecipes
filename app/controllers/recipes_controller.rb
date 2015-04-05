6b72aa13367dc746f9d5bf8f0d24f81c34a6937d
Martin Byrne <marb61a@gmail.com>
2015-04-05 11:17:09 +0000

    added edit recipes form

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -24,6 +24,20 @@ class RecipesController < ApplicationController
     end
   end
   
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+  
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      flash[:success] = "Your recipe was updated successfully"
+      redirect_to recipes_path
+    else
+      render :edit
+    end
+  end
+  
   private 
     def recipe_params
       params.require(:recipe).permit(:name, :summary, :description)
