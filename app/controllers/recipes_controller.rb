d8c51bc802e2a29892a6f5785cfb260022f60470
Martin Byrne <marb61a@gmail.com>
2015-04-06 11:51:02 +0000

    Added image uploading feature

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -40,7 +40,7 @@ class RecipesController < ApplicationController
   
   private 
     def recipe_params
-      params.require(:recipe).permit(:name, :summary, :description)
+      params.require(:recipe).permit(:name, :summary, :description, :picture)
     end
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150331105045) do
+ActiveRecord::Schema.define(version: 20150406104301) do
 
   create_table "chefs", force: true do |t|
     t.string   "chefname"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20150331105045) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "chef_id"
+    t.string   "picture"
   end
 
 end
