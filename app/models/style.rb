28314027a4617ca745e6394528d0a429b9e5b148
Martin Byrne <marb61a@gmail.com>
2015-04-25 17:35:02 +0000

    Changed UI and repaired bug

--- app/models/style.rb
+++ app/models/style.rb
@@ -2,6 +2,6 @@ class Style < ActiveRecord::Base
   
   validates :name, presence: true, length: {minimum: 2, maximum: 25 }
   has_many :recipe_styles
-  has_many :styles, through: :recipe_styles
+  has_many :recipes, through: :recipe_styles
   
 end
--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -10,3 +10,4 @@ if Rails.env.production?
     
   end
 end
+
